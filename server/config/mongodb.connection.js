31122340efd27e52d5195abd7653393ed0758c7b
KRISH DHIMAN <krrishdhiman841@gmail.com>
2024-12-16 18:11:12 +0530

    update mongodb connection

--- server/config/mongodb.connection.js
+++ server/config/mongodb.connection.js
@@ -1,9 +1,14 @@
 import mongoose from 'mongoose'
 import config from './config.js'
 
+const options = {
+    serverSelectionTimeoutMS: 5000,
+    dbName: 'tourism_management_app'
+}
+
 const connectDB = async () => {
     try {
-        mongoose.connect(`${config.mongodb_url}`)
+        await mongoose.connect(`${config.mongodb_url}`, options)
         console.log('mongodb conntected!')
     } catch (error) {
         console.log('mongodb not conntected!')
